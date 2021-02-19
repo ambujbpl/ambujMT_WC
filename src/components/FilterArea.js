0fd0fc5c79f44f35a5ab2947754525effc105a9d
ambujdubey <ambujdubey89@gmail.com>
2021-02-19 13:22:48 +0530

    update filter UI

--- src/components/FilterArea.js
+++ src/components/FilterArea.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './../css/FilterArea.css';
+// Using an ES6 transpiler like Babel
+import Slider from 'react-rangeslider'
+// To include the default styles
+import 'react-rangeslider/lib/index.css'
+// import InputRange from 'react-input-range-minmax-label';
+// import 'react-input-range-minmax-label/lib/css/index.css';
 const ui_data = require('./../utils/FilterArea.json');
 const getHeaderIcons = () => {
 	return (
@@ -8,24 +14,59 @@ const getHeaderIcons = () => {
 		})
 	)
 }
+const handleCheckboxChange = event  => {
+	console.log(event.target.checked);
+}
+
+const createMultipleCheckboxesSwitches = () => {
+	return (
+		ui_data.checkBoxList.map((checkbox,i) => {
+			return (
+				<div className="switch" key={i}>
+					<label>
+						<input type="checkbox" checked={checkbox.isChecked} onChange={(event)=> console.log(event)}/>
+						<span className="lever"></span>						
+					</label>{checkbox.name}
+				</div>
+			)
+		})
+	)
+}
+
 const FilterArea = () => {
 	return(
 		<div>
 			<div>
 				{getHeaderIcons()}
-				<a className="btn-small ml5 verticalAlignMiddle"><i className="material-icons">record_voice_over</i> Assign</a>
+				<a className="waves-effect waves-light btn ml5 verticalAlignMiddle buttonCornerCss"><i className="material-icons left">record_voice_over</i>Assign</a>
 			</div>
-			<div><i className="material-icons">record_voice_over</i> Apply Filter <i className="material-icons right">add</i></div>
-			<div>Difficulty Level</div>
-			<div>
-				<form>      
-		      	<label>
-			      <input name="remember"  type="checkbox"
-			      />
-			      My question bank
-			    </label>
-		      </form>
-		      </div>
+			<div className='mt25 row'><div className='valign-wrapper col s9'><i className="material-icons valign">record_voice_over</i>&nbsp;&nbsp;&nbsp;{ui_data.filterApply}</div><div className='right s3 col'><i className="material-icons circleCss right">add</i></div></div>
+			<div className='mt25'>{ui_data.difficultyLevel}
+				<Slider
+          min={0}
+          max={100}
+          value={40}
+        />
+				<div className='row'>
+					<div className='col s6 themeColor'>{ui_data.easy}</div>
+					<div className='col s6'><span className="right themeColor">{ui_data.hard}</span></div>
+				</div>
+			</div>
+			<div className='mt25 uiSeperator'></div>
+			<div className='mt25'>
+				{createMultipleCheckboxesSwitches()}
+			</div>
+			<div className='mt25 uiSeperator'></div>
+			<div className='mt25 row'>
+				<div className='col s6 themeColor verticalAlignMiddle'><i className="material-icons verticalAlignMiddle">settings</i>&nbsp;&nbsp;&nbsp;{ui_data.settings}</div>
+				<div className='col s6'><i className="material-icons circleCss right">add</i></div>
+			</div>
+			<div className='mt25 uiSeperator'></div>
+			<div className='mt25 row'>
+				<div className='col s6 themeColor verticalAlignMiddle'><i className="material-icons verticalAlignMiddle">insert_invitation</i>&nbsp;&nbsp;&nbsp;{ui_data.schedule}</div>
+				<div className='col s6'><i className="material-icons circleCss right">add</i></div>
+			</div>
+			<div className='mt25 uiSeperator'></div>
 		</div>
 	);
 }
